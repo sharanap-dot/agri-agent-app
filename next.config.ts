11780cb982bfec522b61a488552c2f36d9008d19
Sharan A P <sharanadhikari2006@gmail.com>
2026-06-14 22:57:23 +0530

    Remove PWA temporarily

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
-import withPWA from "next-pwa";
-
 const nextConfig = {
   reactStrictMode: true,
-
-  turbopack: {},
 };
 
-export default withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-})(nextConfig);
+export default nextConfig;
