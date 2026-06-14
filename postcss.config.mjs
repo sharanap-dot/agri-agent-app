f0c78af4a7a7de4230627fa133250e694ae74411
Sharan A P <sharanadhikari2006@gmail.com>
2026-06-14 23:47:36 +0530

    Enable PWA

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
-const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
-};
+const withPWA = require("next-pwa")({
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+});
 
-export default config;
+module.exports = withPWA({
+  reactStrictMode: true,
+});
